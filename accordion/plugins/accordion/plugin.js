2eb37e5f9d40eb7146db353f31a2ca65f9cd9225
Benjamin Alan Alexander <baa2165@.columbia.edu>
2023-08-14 13:11:12 -0400

    animateable

--- accordion/plugins/accordion/plugin.js
+++ accordion/plugins/accordion/plugin.js
@@ -9,13 +9,32 @@ This is updated from "collapsables", circa 2023
 		init: function( editor ) {
 			editor.addCommand( 'accordion', {
 				exec: function( editor ) {
-					var fragment = editor.getSelection().getRanges()[0].extractContents()
-					var container = CKEDITOR.dom.element.createFromHtml("<div/>", editor.document)
+					let fragment = editor.getSelection().getRanges()[0].extractContents();
+					let container = CKEDITOR.dom.element.createFromHtml("<div/>", editor.document);
 					container.setAttribute("class", "collapsible");
-					fragment.appendTo(container)
-					editor.insertElement(container)
+
+					fragment.appendTo(container);
+
+					// Identify the first child element
+					var firstChild = container.getFirst();
+					
+					if (firstChild) {
+						// Create a new div for the remaining content
+						var remainingContentDiv = CKEDITOR.dom.element.createFromHtml("<div/>", editor.document);
+						remainingContentDiv.setAttribute("class", "collapsible-content");
+						
+						// Move the remaining content after the first child to the new div
+						while (firstChild.getNext()) {
+							remainingContentDiv.append(firstChild.getNext());
+						}
+						
+						// Append the new div to the container
+						container.append(remainingContentDiv);
+					}
+
+					editor.insertElement(container);
 				}
-			  });
+			});
 			editor.ui.addButton( 'accordion', {
 				label: 'Accordion',
 				command: 'accordion',
